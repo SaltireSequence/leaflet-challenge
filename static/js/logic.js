61b73982e50cc4b6dfa884d2e54ebd9e4f37c80a
William F <60886603+wtcf90@users.noreply.github.com>
2020-08-11 16:52:21 -0700

    creating styleInfo and colorChoice functions

--- static/js/logic.js
+++ static/js/logic.js
@@ -41,18 +41,21 @@ var baseMaps = {
     "Outdoors": lightMap
 };
 
-// Creating map and passing the darkMap and earthquake_layer as initial load layers
+/* Creating map and passing the darkMap and earthquake_layer as initial load
+layers */
 var myMap = L.map("map", {
     center: [37.09, -95.71],
     zoom: 2,
     layers: [darkMap, earthquake_layer]
 });
 
-// Create a Layer Control + Pass in baseMaps and overlayMaps + Add the Layer Control to the Map
+/* Create a Layer Control + Pass in baseMaps and overlayMaps + Add the Layer
+Control to the Map */
 L.control.layers(baseMaps, overlayMaps).addTo(myMap);
 
 // Harnessing D3 to call and retrieve earthquake data
 d3.json(earthquakesURL, function(earthquakeDataset) {
+
 // Function to calculate the size of the marker, using the earthquak magnitude
   function markerSize(magnitude) {
       if (magnitude === 0) {
@@ -60,3 +63,44 @@ d3.json(earthquakesURL, function(earthquakeDataset) {
       }
       return magnitude * 3;
   }
+
+/* Function with case arguement that determines how the marker is stylised
+based on the magnitude of the earthquake */
+  function styleInfo(feature) {
+      return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: colorChoice(feature.properties.mag),
+        color: "#000000",
+        radius: markerSize(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+      };
+  }
+
+/* Function with case arguement that determines the colour of earthquake marker
+based on the magnitude of the earthquake */
+  function colorChoice(magnitude) {
+      switch (true) {
+      case magnitude > 5:
+          return "#581845";
+      case magnitude > 4:
+          return "#900C3F";
+      case magnitude > 3:
+          return "#C70039";
+      case magnitude > 2:
+          return "#FF5733";
+      case magnitude > 1:
+          return "#FFC300";
+      default:
+          return "#DAF7A6";
+      }
+  }
+
+/* Initializing a GeoJSON layer, that contains the features array on the
+earthquakeDataset */
+  L.geoJSON(earthquakeDataset, {
+      pointToLayer: function(feature, latlng) {
+          return L.circleMarker(latlng);
+      },
+      style: styleInfo,
