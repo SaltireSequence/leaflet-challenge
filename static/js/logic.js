1068528bb30cc1ad216571821b2106fc76fa4a3c
William F <60886603+wtcf90@users.noreply.github.com>
2020-08-11 11:59:54 -0700

    initiating dependencies

---
+++ static/js/logic.js
@@ -0,0 +1,3 @@
+var URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+var mapboxKey = "pk.eyJ1Ijoid2lsbGlhbXRjZm9yc3l0aCIsImEiOiJja2Q5a2d5MGQwaDd3MnhxdzA2ZmM3Z3l4In0.LyUug2qMsb3KqPH5do99yA"
+var mapboxURL = "https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?"
